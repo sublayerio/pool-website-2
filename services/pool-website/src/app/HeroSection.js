82d063d63e7efb802789c171211f1e34d77ab677
Olivier Andriessen <olivier@andev.nl>
2021-07-04 10:22:51 +0000

    feat: 🎸 add telegram handle

--- services/pool-website/src/app/HeroSection.js
+++ services/pool-website/src/app/HeroSection.js
@@ -67,7 +67,8 @@ export default function HeroSection() {
           <div className="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
             <div>
               <a
-                href="#"
+                href="https://t.me/sublayerio"
+                target="_blank"
                 className="inline-flex items-center text-white bg-gray-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
               >
                 <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-primary-600 to-primary-700 rounded-full">
